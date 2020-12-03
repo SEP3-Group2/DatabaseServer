46bd6ce99c677ff89779b4d58e22d0fed596a6d6
Christian Sejer <christian_sejer@hotmail.com>
2020-12-03 11:44:29 +0100

    Refactor

--- src/networking/Handler.java
+++ src/networking/Handler.java
@@ -33,90 +33,108 @@ public class Handler implements Runnable
         }
     }
 
-    @Override public void run()
+    @Override
+    public void run()
     {
         try
         {
             Request request = (Request) inFromClient.readObject();
-            if("GetAllProducts".equals(request.getType())){
+            if ("GetAllProducts".equals(request.getType()))
+            {
                 List<Product> result = databaseManager.getProductManager().getAllProducts();
                 outToClient.writeObject(new Request("GetAllProducts", result));
             }
-            else if ("AddProduct".equals(request.getType())){
+            else if ("AddProduct".equals(request.getType()))
+            {
                 Product product1 = (Product) request.getArg();
-                Product product = databaseManager.getProductManager().addProduct(product1.getTitle(),product1.getCategory(), product1.getDescription(), product1.getPrice());
+                Product product = databaseManager.getProductManager().addProduct(product1.getTitle(), product1.getCategory(), product1.getDescription(), product1.getPrice());
                 outToClient.writeObject(new Request("AddProduct", product));
             }
-            else if("RegisterCustomerUser".equals(request.getType()))
+            else if ("RegisterCustomerUser".equals(request.getType()))
             {
-                databaseManager.getCustomerManager().registerCustomerUser((CustomerUser)request.getArg());
+                databaseManager.getCustomerManager().registerCustomerUser((CustomerUser) request.getArg());
             }
-            else if("RegisterEmployeeUser".equals(request.getType()))
+            else if ("RegisterEmployeeUser".equals(request.getType()))
             {
-                databaseManager.getEmployeeManager().registerEmployeeUser((EmployeeUser)request.getArg());
+                databaseManager.getEmployeeManager().registerEmployeeUser((EmployeeUser) request.getArg());
             }
-            else if("GetAllUsers".equals(request.getType())){
+            else if ("GetAllUsers".equals(request.getType()))
+            {
                 List<CustomerUser> result = databaseManager.getCustomerManager().getAllUsers();
                 outToClient.writeObject(new Request("GetAllUsers", result));
             }
-            else if("GetCustomerUser".equals(request.getType())){
-                CustomerUser result = databaseManager.getCustomerManager().getCustomerUser((String)request.getArg());
+            else if ("GetCustomerUser".equals(request.getType()))
+            {
+                CustomerUser result = databaseManager.getCustomerManager().getCustomerUser((String) request.getArg());
                 outToClient.writeObject(new Request("GetCustomerUser", result));
             }
-            else if("GetEmployeeUser".equals(request.getType())){
-                EmployeeUser result = databaseManager.getEmployeeManager().getEmployeeUser((String)request.getArg());
+            else if ("GetEmployeeUser".equals(request.getType()))
+            {
+                EmployeeUser result = databaseManager.getEmployeeManager().getEmployeeUser((String) request.getArg());
                 outToClient.writeObject(new Request("GetEmployeeUser", result));
             }
-            else if("GetProductById".equals(request.getType())){
-                Product result = databaseManager.getProductManager().getProductById((int)request.getArg());
+            else if ("GetProductById".equals(request.getType()))
+            {
+                Product result = databaseManager.getProductManager().getProductById((int) request.getArg());
                 outToClient.writeObject(new Request("GetProductById", result));
             }
-            else if("GetTitleCategoryPriceFilteredProducts".equals(request.getType())){
-                String [] requests = (String[]) request.getArg();
+            else if ("GetTitleCategoryPriceFilteredProducts".equals(request.getType()))
+            {
+                String[] requests = (String[]) request.getArg();
                 String title = requests[0];
                 String category = requests[1];
                 String price = requests[2];
-                List<Product> result = databaseManager.getProductManager().getTitleCategoryPriceFilteredProducts(title, category,price);
+                List<Product> result = databaseManager.getProductManager().getTitleCategoryPriceFilteredProducts(title, category, price);
                 outToClient.writeObject(new Request("GetTitleCategoryPriceFilteredProducts", result));
             }
-            else if("GetLastProductID".equals(request.getType())){
+            else if ("GetLastProductID".equals(request.getType()))
+            {
                 int returnID = databaseManager.getProductManager().getLastProductID();
                 outToClient.writeObject(new Request("GetLastProductID", returnID));
             }
-            else if("GetAllWarehouseProducts".equals(request.getType())){
-                List<WarehouseProduct> result=databaseManager.getWarehouseProductManager().getAllWarehouseProducts();
+            else if ("GetAllWarehouseProducts".equals(request.getType()))
+            {
+                List<WarehouseProduct> result = databaseManager.getWarehouseProductManager().getAllWarehouseProducts();
                 outToClient.writeObject(new Request("GetAllWarehouseProducts", result));
             }
-            else if("GetStoreWarehouseProduct".equals(request.getType())){
-                List<WarehouseProduct> result=databaseManager.getWarehouseProductManager().getStoreWarehouseProducts((int)request.getArg());
+            else if ("GetStoreWarehouseProduct".equals(request.getType()))
+            {
+                List<WarehouseProduct> result = databaseManager.getWarehouseProductManager().getStoreWarehouseProducts((int) request.getArg());
                 outToClient.writeObject(new Request("GetStoreWarehouseProduct", result));
             }
-            else if("AddWarehouseProduct".equals(request.getType())){
-                WarehouseProduct warehouseProduct1=(WarehouseProduct) request.getArg();
-                WarehouseProduct warehouseProduct=databaseManager.getWarehouseProductManager().addWarehouseProduct(warehouseProduct1.getStoreId(),warehouseProduct1.getProductId(), warehouseProduct1.getQuantity());
+            else if ("AddWarehouseProduct".equals(request.getType()))
+            {
+                WarehouseProduct warehouseProduct1 = (WarehouseProduct) request.getArg();
+                WarehouseProduct warehouseProduct = databaseManager.getWarehouseProductManager().addWarehouseProduct(warehouseProduct1.getStoreId(), warehouseProduct1.getProductId(), warehouseProduct1.getQuantity());
                 outToClient.writeObject(new Request("AddWarehouseProduct", warehouseProduct));
             }
-            else if("GetCartProducts".equals(request.getType())){
+            else if ("GetCartProducts".equals(request.getType()))
+            {
                 int[] requests = (int[]) request.getArg();
                 int productid = requests[0];
                 int quantity = requests[1];
                 List<CartProduct> result = databaseManager.getWarehouseProductManager().GetCartProducts(productid, quantity);
                 outToClient.writeObject(new Request("GetCartProducts", result));
             }
-            else if("AddTransaction".equals(request.getType()))
+            else if ("AddTransaction".equals(request.getType()))
             {
                 Transaction transaction = databaseManager.getTransactionManager().addTransaction((Transaction) request.getArg());
                 outToClient.writeObject(new Request("AddTransaction", transaction));
-            else if("GetAllWPJoin".equals(request.getType())){
-                List<WPJoin> result=databaseManager.getWpJoinManager().getAllWPJoin();
+            }
+            else if ("GetAllWPJoin".equals(request.getType()))
+            {
+                List<WPJoin> result = databaseManager.getWpJoinManager().getAllWPJoin();
                 outToClient.writeObject(new Request("GetAllWPJoin", result));
             }
-            else if("GetStoreWPJoin".equals(request.getType())){
-                List<WPJoin> result=databaseManager.getWpJoinManager().getStoreWPJoin((int)request.getArg());
+            else if ("GetStoreWPJoin".equals(request.getType()))
+            {
+                List<WPJoin> result = databaseManager.getWpJoinManager().getStoreWPJoin((int) request.getArg());
                 outToClient.writeObject(new Request("GetStoreWPJoin", result));
             }
         }
-        catch (IOException | ClassNotFoundException e)
+        catch (IOException |
+                ClassNotFoundException e)
+
         {
             e.printStackTrace();
         }
